549159ebbc9fd1183e04fdc253511bbeefcadde9
vianalin <vlin21@stuy.edu>
2021-01-18 14:12:17 -0500

    list of costs

--- TravelingSalesmen.java
+++ TravelingSalesmen.java
@@ -7,17 +7,38 @@ public class TravelingSalesmen {
 		ArrayList<String> cities = new ArrayList<>(); //result
 
 		
-		while (in.hasNextLine()) {
-			Scanner line = new Scanner(in.nextLine());
-			String from = line.next(); //first column
+		while(in.hasNextLine()) {
+			Scanner column = new Scanner(in.nextLine());
+			String from = column.next(); //first column
 			//System.out.println(from);
-			line.next();
-			String to = line.next(); //second column
+			column.next(); //skip over column w "to"
+			String to = column.next(); //second column
 			//System.out.println(to);
 
 			if(!cities.contains(from)) cities.add(from);
 			if(!cities.contains(to)) cities.add(to); //adding cities that aren't alr in the arraylist
 		}
 		//System.out.println(cities);
+
+		int size = cities.size();
+		int[][] costs = new int[size][size];
+
+		in = new Scanner(new File("cities.txt"));
+
+		while(in.hasNextLine()) {
+			Scanner column = new Scanner(in.nextLine());
+			String to = column.next();
+			column.next(); //skip over column w "to"
+			String from = column.next();
+			column.next(); //skip over column w "="
+			int cost = column.nextInt();
+			//System.out.println(cost);
+
+
+		}
+		
+
+
+
 	}
 }
