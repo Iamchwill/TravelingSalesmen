501adcd4ee09db1903ce30c5077f12ef3255d35a
Tami Takada <tami@mytalos.com>
2021-01-18 14:36:39 -0500

    added yaying's code to distance

--- Distance.java
+++ Distance.java
@@ -2,32 +2,30 @@ import java.util.*;
 
 public class Distance {
   public static void main(String[] args) {
-  //  ArrayList<ArrayList<Integer>> distances = new ArrayList<ArrayList<Integer>>();
+    ArrayList<ArrayList<Integer>> distances = new ArrayList<ArrayList<Integer>>();
     ArrayList<String> allPlaces = new ArrayList<String>();
 
     Scanner scan = new Scanner(System.in);
     while (scan.hasNextLine()) {
       String lineScan = scan.nextLine();
-
       String[] placesAndDistance = lineScan.split(" = ");
       String[] places = placesAndDistance[0].split(" to ");
-
-      int index = 0;
-      if (!allPlaces.contains(places[0])) {
-        index = allPlaces.size();
-        allPlaces.add(places[0]);
-    //    distances.add(new ArrayList<Integer>());
-      } else index = allPlaces.indexOf(places[0]);
-
-      int indexTwo = 0;
-      if (!allPlaces.contains(places[1])) {
-        indexTwo = allPlaces.size();
-        allPlaces.add(places[1]);
-      } else indexTwo = allPlaces.indexOf(places[1]);
-
-      System.out.println(Integer.valueOf(placesAndDistance[1]));
-      System.out.println(allPlaces);
-    //  distances.get(index).set(indexTwo) = Integer.valueOf(placesAndDistance[1]);
     }
+
+    int[][] distances = new int[allPlaces.size()][allPlaces.size()];
+
+    Scanner scan2 = new Scanner(System.in);
+    while (scan2.hasNextLine()) {
+      String lineScan2 = scan2.nextLine();
+
+      String[] sentenceToArray = lineScan2.split(" ");
+
+      distances[allPlaces.indexOf(sentenceToArray[0])]
+                [allPlaces.indexOf(sentenceToArray[2])]
+                = Integer.parseInt(sentenceToArray[4]);
+      distances[allPlaces.indexOf(sentenceToArray[2])]
+                [allPlaces.indexOf(sentenceToArray[0])]
+                = Integer.parseInt(sentenceToArray[4]);
+      }
   }
 }
