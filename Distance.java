5d67223cff2165814d95de28b610cba323c8ee93
vianalin <vlin21@stuy.edu>
2021-01-18 14:19:30 -0500

    shorter scanner

--- Distance.java
+++ Distance.java
@@ -1,33 +1,23 @@
 import java.util.*;
+import java.io.*;
 
 public class Distance {
-  public static void main(String[] args) {
-  //  ArrayList<ArrayList<Integer>> distances = new ArrayList<ArrayList<Integer>>();
-    ArrayList<String> allPlaces = new ArrayList<String>();
+  public static void main(String[] args) throws FileNotFoundException {
+    Scanner in = new Scanner(new File("cities.txt"));
+    ArrayList<String> cities = new ArrayList<>(); //result
 
-    Scanner scan = new Scanner(System.in);
-    while (scan.hasNextLine()) {
-      String lineScan = scan.nextLine();
+    
+    while(in.hasNextLine()) {
+      Scanner column = new Scanner(in.nextLine());
+      String from = column.next(); //first column
+      //System.out.println(from);
+      column.next(); //skip over column w "to"
+      String to = column.next(); //second column
+      //System.out.println(to);
 
-      String[] placesAndDistance = lineScan.split(" = ");
-      String[] places = placesAndDistance[0].split(" to ");
-
-      int index = 0;
-      if (!allPlaces.contains(places[0])) {
-        index = allPlaces.size();
-        allPlaces.add(places[0]);
-    //    distances.add(new ArrayList<Integer>());
-      } else index = allPlaces.indexOf(places[0]);
-
-      int indexTwo = 0;
-      if (!allPlaces.contains(places[1])) {
-        indexTwo = allPlaces.size();
-        allPlaces.add(places[1]);
-      } else indexTwo = allPlaces.indexOf(places[1]);
-
-      System.out.println(Integer.valueOf(placesAndDistance[1]));
-      System.out.println(allPlaces);
-    //  distances.get(index).set(indexTwo) = Integer.valueOf(placesAndDistance[1]);
+      if(!cities.contains(from)) cities.add(from);
+      if(!cities.contains(to)) cities.add(to); //adding cities that aren't alr in the arraylist
     }
+    //System.out.println(cities);
   }
 }
