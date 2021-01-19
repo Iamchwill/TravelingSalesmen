23153d74e5fe2ef15d2a9b0e1634de20d1666206
vianalin <vlin21@stuy.edu>
2021-01-18 19:24:27 -0500

    initializing

--- Distance.java
+++ Distance.java
@@ -5,6 +5,7 @@ public class Distance {
   public static void main(String[] args) throws FileNotFoundException {
     ArrayList<String> cities = findCities("cities.txt");
     int[][] distances = arrayifyDistances(cities, "cities.txt");
+    //System.out.println(paths());
   }
 
   public static ArrayList<String> findCities(String filename) throws FileNotFoundException {
@@ -20,7 +21,6 @@ public class Distance {
       if(!cities.contains(from)) cities.add(from);
       if(!cities.contains(to)) cities.add(to);
     }
-
     return cities;
   }
 
@@ -44,45 +44,18 @@ public class Distance {
     return distances;
   }
 
-
+  public static ArrayList<ArrayList<Integer>> paths() throws FileNotFoundException { //initializing
+    ArrayList<String> cities = findCities("cities.txt");
+    ArrayList<ArrayList<Integer>> paths = new ArrayList<>();
+    for(int i = 0; i < cities.size(); i++) {
+      ArrayList<Integer> start = new ArrayList<>();
+      start.add(i);
+      paths.add(start);
+    }
+    return paths;
+  }
 
 }
 
 
-Start(3):
-  paths = [0]
-      [1]
-      [2]
-  ArrayList<ArrayList<Integer>> paths
-  for(int i = 0; i < cities.size(); i++) 
-    ArrayList<Integer> apath
-    apath.add(i)
-    paths.add(apath)
-
-
-while(path.get(0)).size() < cities.size()
-Iterating 1
-  paths = [0, 1]
-      [0, 2]
-      [1, 0]
-      [1, 2]
-      ...
-  for(ArrayLists in ArrayList<ArrayList> paths)
-    for(int i = 0; i < cities.size(); i++)
-      if(ArrayList doesn't contain i)
-        add i
-
-
-Iterating 2
-  paths = [0, 1, 2]
-      [0, 2, 1]
-      [1, 0, 2]
-      [1, 2, 0]
-      ...
-
-Go through each path and add costs
-for(i : paths)
-  for
-    add
-  if total < min, min = total
 
