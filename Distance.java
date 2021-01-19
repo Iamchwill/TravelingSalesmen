867a1b520b4931e8b6bb162fc8513361d149b0a2
Yaying <yliangli20@stuy.edu>
2021-01-19 10:09:46 -0500

    nothing added; needed to merge

--- Distance.java
+++ Distance.java
@@ -44,45 +44,42 @@ public class Distance {
     return distances;
   }
 
-
-
 }
 
 
-Start(3):
-  paths = [0]
-      [1]
-      [2]
-  ArrayList<ArrayList<Integer>> paths
-  for(int i = 0; i < cities.size(); i++) 
-    ArrayList<Integer> apath
-    apath.add(i)
-    paths.add(apath)
-
-
-while(path.get(0)).size() < cities.size()
-Iterating 1
-  paths = [0, 1]
-      [0, 2]
-      [1, 0]
-      [1, 2]
-      ...
-  for(ArrayLists in ArrayList<ArrayList> paths)
-    for(int i = 0; i < cities.size(); i++)
-      if(ArrayList doesn't contain i)
-        add i
-
-
-Iterating 2
-  paths = [0, 1, 2]
-      [0, 2, 1]
-      [1, 0, 2]
-      [1, 2, 0]
-      ...
-
-Go through each path and add costs
-for(i : paths)
-  for
-    add
-  if total < min, min = total
-
+// Start(3):
+//   paths = [0]
+//       [1]
+//       [2]
+//   ArrayList<ArrayList<Integer>> paths
+//   for(int i = 0; i < cities.size(); i++)
+//     ArrayList<Integer> apath
+//     apath.add(i)
+//     paths.add(apath)
+//
+//
+// while(path.get(0)).size() < cities.size()
+// Iterating 1
+//   paths = [0, 1]
+//       [0, 2]
+//       [1, 0]
+//       [1, 2]
+//       ...
+//   for(ArrayLists in ArrayList<ArrayList> paths)
+//     for(int i = 0; i < cities.size(); i++)
+//       if(ArrayList doesn't contain i)
+//         add i
+//
+//
+// Iterating 2
+//   paths = [0, 1, 2]
+//       [0, 2, 1]
+//       [1, 0, 2]
+//       [1, 2, 0]
+//       ...
+//
+// Go through each path and add costs
+// for(i : paths)
+//   for
+//     add
+//   if total < min, min = total
