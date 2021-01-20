19c43ea1b6bf6ed4cffcb300d36c79bc13ef0d4f
Tami Takada <tami@mytalos.com>
2021-01-19 20:43:54 -0500

    deleted unnecessary utils

--- Utils.java
+++ Utils.java
@@ -2,15 +2,6 @@ import java.util.*;
 
 
 public class Utils {
-  private static Random rng = new Random();
-
-  public static void showRandomResults(ArrayList<String> info) {
-    for (int i = 0; i < info.size(); i++) {
-      System.out.println("Randomized tests #" + (i + 1) + ": FAIL: " + info.get(i));
-    }
-    if (info.size() == 0) System.out.println("Randomized tests: PASS");
-  }
-
   public static void showResults(ArrayList<Boolean> results, String testName) {
     for (int i = 0; i < results.size(); i++) {
       if (results.get(i)) System.out.println(testName + " #" + (i + 1) + ": PASS");
@@ -21,5 +12,4 @@ public class Utils {
   public static String generateFileLine(String cityOne, String cityTwo, int distance) {
     return cityOne + " to " + cityTwo + " = " + distance + "\n";
   }
-
 }
