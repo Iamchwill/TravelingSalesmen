1a13314ebc8794619ba09828b9f453b145c1e208
Tami Takada <tami@mytalos.com>
2021-01-19 20:42:25 -0500

    added tests for arrayifydistances

--- Tester.java
+++ Tester.java
@@ -4,6 +4,93 @@ import java.io.*;
 public class Tester{
   public static void main(String[] args){
     testFindCities();
+    testArrayifyDistance();
+  }
+
+  public static void testArrayifyDistance() {
+    ArrayList<Boolean> results = new ArrayList<Boolean>();
+    File testFile = new File("pasta.txt");
+
+    String[] places = {"Penne", "Fusili", "Rotini"};
+    int[] distances = {80, 121, 57};
+
+    try {
+      FileWriter writer = new FileWriter("pasta.txt");
+      String all = Utils.generateFileLine(places[0], places[1], distances[0]) +
+                    Utils.generateFileLine(places[0], places[2], distances[1]) +
+                    Utils.generateFileLine(places[1], places[2], distances[2]);
+      writer.write(all);
+      writer.close();
+    } catch (IOException e) {
+      results.add(false);
+      System.out.println(e);
+    }
+
+    ArrayList<String> placesList = new ArrayList<String>();
+
+    try {
+      placesList = Distance.findCities("pasta.txt");
+    } catch (FileNotFoundException e) {
+      results.add(false);
+      System.out.println(e);
+    }
+
+    try {
+      int[][] allDistances = Distance.arrayifyDistances(placesList, "pasta.txt");
+      String[] expected = {"[0, 80, 121]", "[80, 0, 57]", "[121, 57, 0]"};
+
+      for (int i = 0; i < allDistances.length; i++) {
+        results.add(Arrays.toString(allDistances[i]).equals(expected[i]));
+      }
+    } catch (FileNotFoundException e) {
+      results.add(false);
+      System.out.println(e);
+    }
+
+    places = new String[]{"Fetuccine", "Bucatini", "Tagliatelle", "Tortellini", "Orecchiette"};
+    distances = new int[]{12, 34, 50, 32, 22, 10, 5, 77, 89, 21};
+
+    try {
+      FileWriter writer = new FileWriter("pasta.txt");
+      String all = "";
+      int index = 0;
+      for (int i = 0; i < places.length - 1; i++) {
+        for (int j = i + 1; j < places.length; j++) {
+          all += Utils.generateFileLine(places[i], places[j], distances[index]);
+          index++;
+        }
+      }
+      writer.write(all);
+      writer.close();
+    } catch (IOException e) {
+      System.out.println(e);
+    }
+
+    try {
+      placesList = Distance.findCities("pasta.txt");
+    } catch (FileNotFoundException e) {
+      System.out.println(e);
+    }
+
+    try {
+      int[][] allDistances = Distance.arrayifyDistances(placesList, "pasta.txt");
+      String[] expected = {
+        "[0, 12, 34, 50, 32]",
+        "[12, 0, 22, 10, 5]",
+        "[34, 22, 0, 77, 89]",
+        "[50, 10, 77, 0, 21]",
+        "[32, 5, 89, 21, 0]"
+      };
+
+      for (int i = 0; i < allDistances.length; i++) {
+        results.add(Arrays.toString(allDistances[i]).equals(expected[i]));
+      }
+    } catch (FileNotFoundException e) {
+      results.add(false);
+      System.out.println(e);
+    }
+
+    Utils.showResults(results, "Test array-ify distance");
   }
 
   public static void testFindCities() {
